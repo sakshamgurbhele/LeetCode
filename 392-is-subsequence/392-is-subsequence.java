1156df7592861215929cdf360b306be9f7ef5aa7
SA KSH AM <sakshamm@duck.com>
2022-03-02 22:54:30 +0530

    Time: 1 ms (88.36%), Space: 41.6 MB (42.22%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+        if(s.length() == 0) return true;
+        int count = 0;
+        for(int i = 0; i<=t.length()-1; i++){
+            if(s.charAt(count) == t.charAt(i)){
+                count++;
+                if(count == s.length()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
