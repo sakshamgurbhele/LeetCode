9ab6e3504bce567b2c433f5f7c077dfb704a62a5
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2022-03-02 21:43:29 +0530

    subarray maximum using kadane algo☄️

--- Arrays/MaximumSubarray.java
+++ Arrays/MaximumSubarray.java
@@ -2,11 +2,23 @@ package com.saksham.Arrays;
 
 public class MaximumSubarray {
     public static void main(String[] args) {
-        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
-        int sum = 9;
-        for(int i = 0; i<=nums.length-1; i++){
-            sum = sum + nums[i];
+
+        //Kadane's Algorithm
+
+        int[] nums = {1};
+        int curr_max = 0;
+        int overall_max = nums[0];
+        int temp = nums[0];
+        for(int i = 0; i<= nums.length-1; i++){
+            if(curr_max > 0){
+                curr_max = curr_max + nums[i];
+            }else{
+                curr_max = nums[i];
+            }
+            if(curr_max > overall_max){
+                overall_max = curr_max;
+            }
         }
-        System.out.println(sum-9);
+        System.out.println(overall_max + " This is MaximumSubArray  " );
     }
 }
